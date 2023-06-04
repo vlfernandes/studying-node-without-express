67a46c45ae3b21b851201bbe9eed76243ad1f5dd
Vitor Luis Fernandes <vitor-l-f@hotmail.com>
2023-06-04 01:19:58 -0300

    feat: add getUserById with URL params

--- src/index.js
+++ src/index.js
@@ -8,12 +8,23 @@ const server = http.createServer((request, response) => {
 
   console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split("/").filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
   const route = routes.find(
-    (routeObj) => routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    (routeObj) => routeObj.endpoint === pathname && routeObj.method === request.method
   );
 
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
     route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
